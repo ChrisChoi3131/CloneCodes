bc7fcc37c36468511ad22d38a36e30809b040581
Chris Choi <chrischoi3131@gmail.com>
2022-06-03 20:18:56 -0700

    Add ImageComponent

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,10 +1,13 @@
 import { PageComponent } from "./components/page.js";
+import { ImageComponent } from "./components/items/image.js";
 
 class App {
   private readonly page: PageComponent;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
+    const image = new ImageComponent("test image", "https://picsum.photos/600/300");
+    image.attachTo(appRoot, "beforeend");
   }
 }
 new App(document.querySelector(".document")! as HTMLElement);
