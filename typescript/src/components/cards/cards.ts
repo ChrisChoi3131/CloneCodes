f62d4b3b2c6540cc5aa3d78ee399d616d586f171
Chris Choi <chrischoi3131@gmail.com>
2022-06-06 02:16:33 -0700

    Add 'CardItemContainer', Close btn

--- typescript/src/components/cards/cards.ts
+++ typescript/src/components/cards/cards.ts
@@ -1,6 +1,48 @@
-import { BaseComponent } from "../common/conponents.js";
-export class CardsComponent extends BaseComponent<HTMLUListElement> {
+import { BaseComponent, Component } from "../common/conponents.js";
+
+export interface Composable {
+  addChild(child: Component): void;
+}
+type OnCloseListener = () => void;
+export class CardsComponent extends BaseComponent<HTMLElement> implements Composable {
   constructor() {
-    super('<ul class="page">Cards Component</ul>');
+    super('<ul class="cards"></ul>');
+  }
+  addChild(section: Component) {
+    const item = new CardItemComponent();
+    item.addChild(section);
+    item.attachTo(this.element, "beforeend");
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });
+  }
+}
+
+class CardItemComponent extends BaseComponent<HTMLElement> implements Composable {
+  private closeListener?: OnCloseListener;
+  constructor() {
+    super(
+      `
+      <li class="card-item">
+        <div class="card-item__body">
+          <div class="card-item__controls">
+            <button class="close">&times;</button>
+          </div>
+        </div>
+      </li>
+      `
+    );
+    const li = this.element;
+    li.addEventListener("click", (e) => {
+      const element = e.target as HTMLElement;
+      if (element.tagName === "BUTTON" && element.className === "close") li.remove();
+    });
+  }
+  addChild(child: Component) {
+    const container = this.element.querySelector(".card-item__body")! as HTMLElement;
+    child.attachTo(container);
+  }
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
   }
 }
--- typescript/src/components/layouts/main.ts
+++ typescript/src/components/layouts/main.ts
@@ -5,23 +5,27 @@ import { CardsComponent } from "./../cards/cards.js";
 import { ImageComponent } from "./../cards/items/image.js";
 
 export class Main {
-  private readonly card: CardsComponent;
+  private readonly cards: CardsComponent;
   private readonly main: HTMLElement;
   constructor() {
     this.main = document.querySelector(".document")!;
-    this.card = new CardsComponent();
-    this.card.attachTo(this.main);
+    this.cards = new CardsComponent();
+    this.cards.attachTo(this.main);
     const image = new ImageComponent("TEST Image", "https://picsum.photos/600/300");
-    image.attachTo(this.main, "beforeend");
-    const note = new NoteComponent(
-      "title~~",
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure commodi aliquam, eligendi molestiae aperiam corrupti architecto beatae distinctio a pariatur fugit unde maxime non sed quas eaque modi voluptates ipsam!"
-    );
-    note.attachTo(this.main);
-    const todo = new TodoComponent("일합시다 !!!", "123123131231");
-    todo.attachTo(this.main);
-    const video = new VideoComponent("video title!!!", "https://www.youtube.com/embed/GWdm1Ln9KRs");
-    video.attachTo(this.main);
+    // const image1 = new ImageComponent("TEST Image", "https://picsum.photos/600/300");
+    this.cards.addChild(image);
+    // this.cards.addChild(image);
+    // this.cards.addChild(image);
+    // image.attachTo(this.main, "beforeend");
+    // const note = new NoteComponent(
+    //   "title~~",
+    //   "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure commodi aliquam, eligendi molestiae aperiam corrupti architecto beatae distinctio a pariatur fugit unde maxime non sed quas eaque modi voluptates ipsam!"
+    // );
+    // note.attachTo(this.main);
+    // const todo = new TodoComponent("일합시다 !!!", "123123131231");
+    // todo.attachTo(this.main);
+    // const video = new VideoComponent("video title!!!", "https://www.youtube.com/embed/GWdm1Ln9KRs");
+    // video.attachTo(this.main);
   }
   attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
     parent.insertAdjacentElement(position, this.main);
