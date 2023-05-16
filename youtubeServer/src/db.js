7b57134ac22efdf214aef0d0f027de0ecf8e0fed
Chris Choi <chrischoi3131@gmail.com>
2023-05-16 16:40:16 -0700

    delete Folder

--- youtubeServer/src/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose"
-mongoose.connect(process.env.DB_URL);
-mongoose.connection.on('error', err => {
-  console.log("❌ error connected to db")
-});
-mongoose.connection.once('open', () => {
-  console.log("✅ connected to db")
-});
