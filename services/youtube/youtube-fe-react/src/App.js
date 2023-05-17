f207830b8b8ded2f8ab6b5ea81eaaa1ba3aa36a7
Chris Choi <chrischoi3131@gmail.com>
2023-05-16 20:12:37 -0700

    init react-router

--- services/youtube/youtube-fe-react/src/App.js
+++ services/youtube/youtube-fe-react/src/App.js
@@ -1,10 +1,18 @@
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
