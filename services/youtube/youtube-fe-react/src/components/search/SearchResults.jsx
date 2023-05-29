bf09e7f072aee5faaa9f47423aa7ff190a5e07b0
Chris Choi <chrischoi3131@gmail.com>
2023-05-28 20:26:21 -0700

    Add call local.json for test

--- services/youtube/youtube-fe-react/src/components/search/SearchResults.jsx
+++ services/youtube/youtube-fe-react/src/components/search/SearchResults.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function SearchResults() {
   let { videoId } = useParams();
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    async function fetchSearch() {
+      setVideos([]);
+      const searchResponse = await fetch(`/videos/${videoId ? 'search' : 'popular'}.json`);
+      if (searchResponse.ok && !ignore) {
+        const { items } = await searchResponse.json();
+        setVideos(items);
+      }
+    }
+    let ignore = false;
+    fetchSearch();
+    return () => {
+      ignore = true;
+    }
+  }, [videoId]);
   return (
     <>
-      {videoId ? videoId : 'Hot~~🔥'}
+      {videos.map(({ snippet }) => {
+        return snippet.title
+      })}
     </>
   )
 }
