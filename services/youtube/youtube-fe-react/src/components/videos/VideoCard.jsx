0da49d246bb8fb743dfa429d1fc118e164668ecd
Chris Choi <chrischoi3131@gmail.com>
2023-05-31 18:18:07 -0700

    Add click handler

--- services/youtube/youtube-fe-react/src/components/videos/VideoCard.jsx
+++ services/youtube/youtube-fe-react/src/components/videos/VideoCard.jsx
@@ -1,9 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import { formatTimeAgo } from '../../util/date';
 export default function VideoCard({ video }) {
-  const { snippet } = video;
-  const { publishedAt, title, channelTitle, thumbnails } = snippet
+  const { id: videoId, snippet } = video;
+  const { publishedAt, title, channelTitle, thumbnails } = snippet;
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate(`/watch/${videoId}`)
+  }
+
   return (
-    <li className=''>
+    <li className='cursor-pointer' onClick={handleClick} >
       <img className='w-full rounded-xl' alt={title} src={thumbnails.medium.url} />
       <h3 className='my-1 font-semibold line-clamp-2'>{title}</h3>
       <p className='text-sm opacity-80'>{channelTitle}</p>
